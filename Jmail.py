55c2894ab116347f7314c08d8e114869b1a94dfb
Jeremy Cornish-Ford <jford209@yahoo.com>
2016-10-26 14:18:57 -0700

    Added main code
    Revamped code, and added sys module, and base64 for password obfuscation

--- Jmail.py
+++ Jmail.py
@@ -1,18 +1,19 @@
 
 import smtplib
+import sys
 import base64
 
 
 
 #Receiver
-to = ''
+to = base64.b64decode('amZvcmQyMDlAeWFob28uY29t')
 #Sender
 fro = email 
 
 
 # Google SMTP Server Login Information
-email = base64.b4decode('amF5ZjQ2NDBAZ21haWwuY29t')
-pswd = base64.b4decode('YmluYXJ5c2VhcmNodHJlZQ==')
+email = base64.b64decode('amF5ZjQ2NDBAZ21haWwuY29t')
+pswd = base64.b64decode('YmluYXJ5c2VhcmNodHJlZQ==')
  
 #Email message variable to send
 message = 'Hello, my name is Jeremy this is a automate message, to test out a script, please acknowledge that you received it \n Thank you, \n Jeremy Ford'
@@ -22,28 +23,25 @@ message = 'Hello, my name is Jeremy this is a automate message, to test out a sc
 # SSL port: 465
 #TLS port: 587
 try:
-	smtpobj = smtplib.SMTP('Smtp.gmail.com' 587)
-	#Identify myself to  the server(4 way handshake)
-	smtobj.ehlo()
-	smtpobj.starttls()
-	smtpobj.Login(email,pswd)
-	smtobj.quit() 
+	def send_mail():
+		#Instantiate smtplib object 
+		smtpobj = smtplib.SMTP('Smtp.gmail.com' 587)
+		#Identify myself to  the server, return tuple from server
+		smtobj.ehlo()
+		# Prepare TLS encrypted connection
+		smtpobj.starttls()
+		#Login to SMTP server
+		smtpobj.login(email,pswd)
+		#Send mail to the reciepient
+		smtobj.sendmail(fro, to, message) 
+		# End the connection
+		smtobj.quit() 
 except:
-	print "Server could'nt authenticate the client"
-	sys.quit
+		print "Server could'nt authenticate the client"
+		sys.quit
 
-#Put Server in TLS encrypted mode (encrypt all data sent to and received from server)
-smtpobj.starttls()
 
-#Login to the SMTP server
-smtpobj.Login(email, pswd)
-
-#Send mail to the reciepient
-smtobj.sendmail(fro, [email1,email2], message) 
-
-#Close the session and connection
-smtobj.quit() 
 
 #Run the program in terminal, if imported don't 
 if '__name__' == '__main__':
-    raw_input()
+    send_mail()
